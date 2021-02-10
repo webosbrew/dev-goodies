f8b9b05e08905a545729998bbce5edc752c4812f
Mariotaku <mariotaku.lee@gmail.com>
2021-02-10 18:47:57 +0900

    Update reset-devmode-timer.sh

--- reset-devmode-timer.sh
+++ reset-devmode-timer.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-sessionToken=$(ssh -i tv_webos -p 9922 prisoner@192.168.4.104 cat /var/luna/preferences/devmode_enabled)
+SSH_KEYFILE="/root/.ssh/tv_webos"
+TV_HOST="192.168.4.104"
+
+sessionToken=$(ssh -i ${SSH_KEYFILE} -p 9922 prisoner@${TV_HOST} cat /var/luna/preferences/devmode_enabled)
 
 checkSession=$(curl --max-time 3 -s "https://developer.lge.com/secure/ResetDevModeSession.dev?sessionToken=${sessionToken}")
