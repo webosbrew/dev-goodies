b04b2c0d850a3eac35c59d20f2af199e0f2b8c60
Mariotaku <mariotaku.lee@gmail.com>
2021-02-18 03:53:37 +0900

    Caches dev token
    ... so next time when TV is off, it can still update devmode session

--- reset-devmode-timer.sh
+++ reset-devmode-timer.sh
@@ -3,6 +3,18 @@
 SSH_KEYFILE="/root/.ssh/tv_webos"
 TV_HOST="192.168.4.104"
 
-sessionToken=$(ssh -i ${SSH_KEYFILE} -p 9922 prisoner@${TV_HOST} cat /var/luna/preferences/devmode_enabled)
+sessionToken=$(ssh -i ${SSH_KEYFILE} -o ConnectTimeout=3 -p 9922 prisoner@${TV_HOST} cat /var/luna/preferences/devmode_enabled)
+if [ -z "${sessionToken}" ]; then
+  sessionToken=$(cat /tmp/webos_devmode_token.txt)
+else
+  echo ${sessionToken} > /tmp/webos_devmode_token.txt
+fi
+
+if [ -z "${sessionToken}" ]; then
+  echo "Unable to get token" >&2
+  exit 1
+fi
 
 checkSession=$(curl --max-time 3 -s "https://developer.lge.com/secure/ResetDevModeSession.dev?sessionToken=${sessionToken}")
+
+echo $checkSession
